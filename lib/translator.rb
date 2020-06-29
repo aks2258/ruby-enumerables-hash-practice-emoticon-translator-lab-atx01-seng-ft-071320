c4098e6a57d24f7eda0a5e5a36589232a4a13689
Arnie Serrano <arniekidd@gmail.com>
2020-06-29 18:20:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons_hash = YAML.load_file(file_path)
+  binding.pry
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+
 end
