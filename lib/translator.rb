b0431c627f11d2a38ea069fd4fbb1cc98e2f7c49
Arnie Serrano <arniekidd@gmail.com>
2020-06-29 18:49:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   final_emoticon_hash={}
   
   emoticons_hash.each do |emotion_word, faces|
-    final_emoticon_hash[emoticon_word]={}
+    final_emoticon_hash[emotion_word]={}
       final_emoticon_hash[emotion_word][:english]=faces[0]
       final_emoticon_hash[emotion_word][:japanese]=faces[1]
   end
