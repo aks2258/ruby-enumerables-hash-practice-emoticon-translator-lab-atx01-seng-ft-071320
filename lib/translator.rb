79d505f020f616a7ab1d8c557890ca9898240ae0
Arnie Serrano <arniekidd@gmail.com>
2020-06-29 18:51:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   library=load_library(file_path)
   library.map do |word, translations|
-    
+    if translations
   end
 end
 
