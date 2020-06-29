bdcf3a2d0d4b521388c221d91a4482636de28a86
Arnie Serrano <arniekidd@gmail.com>
2020-06-29 18:53:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return translations[:japanese]
     end
   end
-  "Sorry. Could not find the emoticon"
+  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
